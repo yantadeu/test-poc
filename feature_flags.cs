d2f3599feac5197975bb69e98bf77ef60ea2b7ff
michele.marques <michele.marques@mrv.com.br>
2021-07-30 07:35:47 -0300

    Ajustes azure functions

--- feature_flags.cs
+++ feature_flags.cs
@@ -1,5 +1,3 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,29 +5,35 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Collections.Generic;
+using test_poc.Models;
 
 namespace feature_flags
 {
     public static class feature_flags
     {
-        [FunctionName("feature_flags")]
-        public static async Task<IActionResult> Run(
+       [FunctionName("feature_flags")]
+        public static async Task<JsonResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            var config = new ConfigurationBuilder()
+                .AddAzureAppConfiguration("Endpoint=https://appcs-plataformadigital-qas.azconfig.io;Id=pzi5-le-s0:yuMvoKnxtcRqUqWYWYss;Secret=nEZtPEYtgPS57jc8dBl6w6zORYZTdX+vuETdky54deg=")
+                .Build();
 
-            string name = req.Query["name"];
+            var lstConfigs = config.AsEnumerable().ToList();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            List<FeatureFlag> lstFeatureFlags = new List<FeatureFlag>();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            foreach (var item in lstConfigs)
+            {
+                if (item.Key.Contains(".appconfig.featureflag"))
+                    lstFeatureFlags.Add(JsonConvert.DeserializeObject<FeatureFlag>(item.Value));
+            }
 
-            return new OkObjectResult(responseMessage);
+            return new JsonResult(lstFeatureFlags);
         }
     }
 }
